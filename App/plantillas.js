c8304ce19bb084a3ea14402db868f199a0489fdb
Andres Castaño <134648791+Felipyan19@users.noreply.github.com>
2023-12-12 13:31:10 -0500

    Update plantillas.js -> invitacion_conectado

--- App/plantillas.js
+++ App/plantillas.js
@@ -9,37 +9,12 @@ function cargarPlantillas() {
     // const vector = inputtextarea.value.split('\n').map(Number);
     // const vectorJSON = vector.map((numero, posicion) => ({ numero, posicion, imagen: imagen, plantilla: plantilla }));
 
-    // console.log(vectorJSON);
-    console.log(selectPlantilla.value);
-    if(selectPlantilla.value === "bienestar"){
-        plantilla="bienestar";
+    // console.log(vectorJSON)
+         if(selectPlantilla.value === "invitacion_conectado"){
+        plantilla="invitacion_conectado";
         imgPlantilla.style.display = "block";
-        imgPlantilla.src = "https://i.ibb.co/fqzVVK0/Screenshot-5.png"
-        imagen = "";
-    }
-    if(selectPlantilla.value === "newapp"){
-        plantilla="newapp";
-        imgPlantilla.style.display = "block";
-        imgPlantilla.src = "https://i.ibb.co/t4dmCn5/Screenshot-4.png"
-        imagen = "";
-    }
-     if(selectPlantilla.value === "masivolive"){
-        plantilla="masivolive";
-        imgPlantilla.style.display = "block";
-        imgPlantilla.src = "https://i.ibb.co/LCF7qLd/Piezalive-Unisanitas.jpg"
-        imagen = "https://i.ibb.co/LCF7qLd/Piezalive-Unisanitas.jpg";
-    }
-         if(selectPlantilla.value === "admitidosvencidos"){
-        plantilla="admitidosvencidos";
-        imgPlantilla.style.display = "block";
-        imgPlantilla.src = "https://i.ibb.co/FYDFF71/RECIBOSVENCIDOS.jpg"
-        imagen = "https://i.ibb.co/FYDFF71/RECIBOSVENCIDOS.jpg";
-    }
-         if(selectPlantilla.value === "recibosvigentes"){
-        plantilla="recibosvigentes";
-        imgPlantilla.style.display = "block";
-        imgPlantilla.src = "https://i.ibb.co/4gGCvxD/Admitidos-Vigentes.jpg"
-        imagen = "https://i.ibb.co/4gGCvxD/Admitidos-Vigentes.jpg";
+        imgPlantilla.src = "https://i.ibb.co/YRscN44/Comunicado-invitacion.png"
+        imagen = "https://i.ibb.co/YRscN44/Comunicado-invitacion.png";
     }
 };
 
