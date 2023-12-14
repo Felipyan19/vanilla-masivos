91c76845c9b6ae882058e456fc18576ef7ca63c5
Andres Castaño <134648791+Felipyan19@users.noreply.github.com>
2023-12-14 09:57:45 -0500

    Update plantillas.js

--- App/plantillas.js
+++ App/plantillas.js
@@ -10,12 +10,12 @@ function cargarPlantillas() {
     // const vectorJSON = vector.map((numero, posicion) => ({ numero, posicion, imagen: imagen, plantilla: plantilla }));
 
     // console.log(vectorJSON)
-    // if(selectPlantilla.value === "invitacion_conectado"){
-    //     plantilla="invitacion_conectado";
-    //     imgPlantilla.style.display = "block";
-    //     imgPlantilla.src = "https://i.ibb.co/YRscN44/Comunicado-invitacion.png"
-    //     imagen = "https://i.ibb.co/YRscN44/Comunicado-invitacion.png";
-    // }
+    if(selectPlantilla.value === "admitidosvencidos"){
+        plantilla="admitidosvencidos";
+        imgPlantilla.style.display = "block";
+        imgPlantilla.src = "https://i.ibb.co/YRscN44/Comunicado-invitacion.png"
+        imagen = "https://i.ibb.co/YRscN44/Comunicado-invitacion.png";
+    }
         if(selectPlantilla.value === "cuidado-enfermeria"){
         plantilla="cuidado";
         imgPlantilla.style.display = "block";
